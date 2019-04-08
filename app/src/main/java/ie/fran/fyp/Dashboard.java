f3b92ef4d13c391ffcafbc388187e35051eb3d5f
Fran <frmm97@gmail.com>
2019-04-08 09:08:59 +0100

    Todo list is working

--- app/src/main/java/ie/fran/fyp/Dashboard.java
+++ app/src/main/java/ie/fran/fyp/Dashboard.java
@@ -15,6 +15,7 @@ import android.widget.RelativeLayout;
 
 import ie.fran.fyp.Focus_On.Focus_On_Activity;
 import ie.fran.fyp.Notes.Note_Activity;
+import ie.fran.fyp.Planner.Planner_Activity;
 import ie.fran.fyp.ToDo.ToDo_Activity;
 
 public class Dashboard extends AppCompatActivity
---
+++ app/src/main/java/ie/fran/fyp/Planner/Planner_Activity.java
@@ -0,0 +1,15 @@
+package ie.fran.fyp.Planner;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import ie.fran.fyp.R;
+
+public class Planner_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_planner);
+    }
+}
