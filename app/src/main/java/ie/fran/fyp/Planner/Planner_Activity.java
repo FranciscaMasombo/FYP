1403824db4ef7fdbca1bf3a7afcb65a05248ded9
Fran <frmm97@gmail.com>
2019-04-09 19:10:59 +0100

    Todo list is working

--- app/src/main/java/ie/fran/fyp/Planner/Planner_Activity.java
+++ app/src/main/java/ie/fran/fyp/Planner/Planner_Activity.java
@@ -1,15 +1,46 @@
 package ie.fran.fyp.Planner;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.RelativeLayout;
 
+import ie.fran.fyp.Dashboard;
+import ie.fran.fyp.Focus_On.Focus_On_Activity;
 import ie.fran.fyp.R;
 
 public class Planner_Activity extends AppCompatActivity {
+    RelativeLayout monday, tuesday, wednesday, thursday, friday , saturday, sunday;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_planner);
+
+        monday = findViewById(R.id.monday);
+        tuesday = findViewById(R.id.tuesday);
+        wednesday = findViewById(R.id.wednesday);
+        thursday = findViewById(R.id.thursday);
+        friday = findViewById(R.id.friday);
+        saturday = findViewById(R.id.saturday);
+        sunday = findViewById(R.id.sunday);
+
+        monday.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Planner_Activity.this, Monday.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+                startActivity(intent);
+            }
+        });
+
+
+
     }
+
+
 }
